f533bb54c50473b6236cdf1eaf36a14a6afc755f
fraxier <fraxier95@outlook.com>
2024-02-01 12:00:04 +1100

    removed progress bar, added router update on load

--- app/components/FaceRecognition/FaceRecognition.js
+++ app/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,19 @@
-import { CircularProgress } from "@nextui-org/react"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 
-export default function FaceRecognition({imgURL, boxes, showLoading}) {
+export default function FaceRecognition({imgURL}) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('#pg-bottom')
+    
+  }, [])
 
   return (
-    <div className="relative max-w-screen-sm mx-auto">
-      {showLoading ? <CircularProgress label="Feeling age vibe..." className="mx-auto" /> : ""}
-      
-      <img id="image" src={imgURL} />
+    <div className="relative max-w-screen-sm mx-auto my-4">      
+      <img className="my-4" id="image" src={imgURL} />
     </div>
   )
 }
